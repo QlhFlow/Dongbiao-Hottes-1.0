060cab77e60dfc51d43df4587e607ce876f0e6a6
selina1202 <yuze1202@sina.com>
2017-03-15 17:02:06 +0800

    one

---
+++ js/style.js
@@ -0,0 +1,9 @@
+//横屏
+    	window.addEventListener("onorientationchange" in window ? "orientationchange" : "resize", function() {
+        if (window.orientation === 180 || window.orientation === 0) {
+            alert('竖屏状态！');
+        }
+        if (window.orientation === 90 || window.orientation === -90 ){
+            alert('横屏状态！');
+        } 
+    }, false);
