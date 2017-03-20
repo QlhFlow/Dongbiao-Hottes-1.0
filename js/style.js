34ccdcd9eb96c7954fb0436102758ccc730a3a3f
aimee1608 <shuigongqian@sina.com>
2017-03-20 13:43:46 +0800

    大多数

--- js/style.js
+++ js/style.js
@@ -49,39 +49,39 @@
         }
 
     }, false);
-var w = window.innerWidth;
-var h = window.innerHeight;
-$(function(){
-    init();
-});
-
-$("#portrait").css("display","none");
-$("body").css({"width":w,"height":h});
-document.addEventListener("WeixinJSBridgeReady", function () {
-    audioAutoPlay('musicStar');
-}, false);
-var firstTouch = true;
-$('body').bind("touchstart",function(e){
-    if ( firstTouch ) {
-        firstTouch = false;
-        musicStar.play();
-    }else{
-        return;
-    }
-});
-//动画开始播放音乐
-musicStar.load();
-musicStar.src="video/Echo.mp3";
-musicStar.play();
-$(".open").click(function(){
-    musicStar.pause();
-    $(this).css("display","none");
-    $(".clock").css("display","block");
-    $('.btn-music').removeClass('open-music');
-});
-$(".clock").click(function(){
-    musicStar.play();
-    $(this).css("display","none");
-    $(".open").css("display","block");
-    $('.btn-music').addClass('open-music');
-});
+//var w = window.innerWidth;
+//var h = window.innerHeight;
+//$(function(){
+//    init();
+//});
+//
+//$("#portrait").css("display","none");
+//$("body").css({"width":w,"height":h});
+//document.addEventListener("WeixinJSBridgeReady", function () {
+//    audioAutoPlay('musicStar');
+//}, false);
+//var firstTouch = true;
+//$('body').bind("touchstart",function(e){
+//    if ( firstTouch ) {
+//        firstTouch = false;
+//        musicStar.play();
+//    }else{
+//        return;
+//    }
+//});
+////动画开始播放音乐
+//musicStar.load();
+//musicStar.src="video/Echo.mp3";
+//musicStar.play();
+//$(".open").click(function(){
+//    musicStar.pause();
+//    $(this).css("display","none");
+//    $(".clock").css("display","block");
+//    $('.btn-music').removeClass('open-music');
+//});
+//$(".clock").click(function(){
+//    musicStar.play();
+//    $(this).css("display","none");
+//    $(".open").css("display","block");
+//    $('.btn-music').addClass('open-music');
+//});
